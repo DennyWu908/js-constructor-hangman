2849296ba8c4283e38a5fc626503e5eef75efb5c
Denny Wu <denny.wu87@gmail.com>
2017-11-16 14:40:47 -0800

    Created gitignore and package.json files

--- hangman_game.js
+++ hangman_game.js
@@ -1,2 +1,5 @@
 var Word = require("./word.js");
 var Letter = require("./letter.js");
+
+var answer_bank = ["skateboard", "rocket", "plum", "phone", "xylophone"];
+var game_answer = answer_bank[(Math.floor(Math.random() * answer_bank.length))]
