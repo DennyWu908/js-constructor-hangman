acea6458c2bebe80b62018febb04a22c409a1f9b
Denny Wu <denny.wu87@gmail.com>
2017-11-16 14:46:39 -0800

    Writing logic for hangman game

--- hangman_game.js
+++ hangman_game.js
@@ -1,5 +1,9 @@
 var Word = require("./word.js");
 var Letter = require("./letter.js");
+var inquirer = require("inquirer");
 
 var answer_bank = ["skateboard", "rocket", "plum", "phone", "xylophone"];
-var game_answer = answer_bank[(Math.floor(Math.random() * answer_bank.length))]
+var game_answer = answer_bank[(Math.floor(Math.random() * answer_bank.length))];
+
+var mystery_word = new Word(game_answer);
+console.log(mystery_word)
